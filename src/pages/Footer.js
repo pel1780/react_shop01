f05f5a7911e5afe202bbdb02b845256c93994ec1
pel1780 <pel1780@github.com>
2023-03-08 11:19:41 +0900

    footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -5,11 +5,25 @@ const FooterWrapper = styled.footer`
 background: #333;
 color: #fff;
 `
-
+const FooterTop = styled.div`
+`
+const FooterBottom = styled.div`
+`
+const FInner = styled.div`
+width: 1200px;
+margin: 0 auto;
+display: flex;
+justify-content: space-between;
+`
 const Footer = ({ DEFAULT, NAV }) => {
     return (
         <FooterWrapper>
-            <GlobalNav NAV={NAV} />
+            <FooterTop>
+                <FInner></FInner>
+            </FooterTop>
+            <FooterBottom>
+                <FInner></FInner>
+            </FooterBottom>
         </FooterWrapper>
     )
 }
