3514db4d22bf54ecb146dfe75c1821f3c39fa524
pel1780 <pel1780@github.com>
2023-03-08 12:32:08 +0900

    to top

--- src/pages/ToTop.js
+++ src/pages/ToTop.js
@@ -2,21 +2,47 @@ import { BsFillArrowUpCircleFill } from "react-icons/bs";
 import styled from "styled-components";
 import gsap from "gsap";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const ToTopWrapper = styled.div`
     position: fixed;
     inset: auto 50px 50px auto;
     font-size: 40px;
     color: tan;
+    opacity: 0;
+    visibility: hidden;
+    transition: 0.5s;
+
+    &.on{
+        opacity: 1;
+        visibility: visible;
+    }
 `
 const ToTop = () => {
-    useEffect(() => {
+    const [scr, setScr] = useState(0);
 
-    }, [])
-    // gsap.registerPlugin(Scroll)
+    const ToTopHandler = () => {
+        gsap.to(window, { duration: 0.5, scrollTo: 0 });
+    }
+
+    const scrollHander = () => {
+        let sct = window.scrollY;
+        setScr(sct);
+    }
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollToPlugin);
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener('scroll', scrollHander);
+        return () => {
+            window.removeEventListener('scroll', scrollHander);
+        }
+    }, []);
+
     return (
-        <ToTopWrapper onClick={() => { console.log(11) }}>
+        <ToTopWrapper onClick={ToTopHandler} className={scr > 400 ? 'on' : ''}>
             <BsFillArrowUpCircleFill />
         </ToTopWrapper>
     )
